859f0bdd1115e7d649d9a93501439baa7c460a8b
Rohit Prajapat <101388814+Rohit20033@users.noreply.github.com>
2023-07-19 05:59:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ const app = express()
 
 
 app.use(express.json())
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 
